e99125c0e8da373cab0d4677957b846b9666240e
IvanR96 <irios96@hotmail.com>
2024-06-21 23:48:41 -0700

    random nums and images added

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-document.querySelector('div');
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+let randomImage = 'dice' + randomNumber1 + '.png';
+
+let randomImage2 = 'dice' + randomNumber2 + '.png';
+
+document.querySelector('.img1').setAttribute('src', randomImage);
+
+document.querySelector('.img2').setAttribute('src', randomImage2);
+
+if (randomNumber1 > randomNumber2){
+    
+}
